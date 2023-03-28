68e75bffde4e9079d87ec53fe86f6cc91b857dd2
sunli <33113170+sunli8539@users.noreply.github.com>
2023-03-28 15:13:49 +0800

    Create CommonConstant.java

---
+++ src/main/java/com/smartai/common/constants/CommonConstant.java
@@ -0,0 +1,10 @@
+package com.smartai.common.constants;
+
+public class CommonConstant {
+    public static final String SUCCESS_CODE = "200";
+    public static final String SUCCESS_MSG = "success";
+
+    public static final String FAILURE_CODE = "500";
+    public static final String FAILURE_MSG = "failure";
+
+}
