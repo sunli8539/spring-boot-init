7d6c1340e9f9774686a8ede672d4acac12ae82c3
sunli <33113170+sunli8539@users.noreply.github.com>
2023-03-28 15:42:10 +0800

    Create StudentParamVO.java

---
+++ src/main/java/com/smartai/student/vo/StudentParamVO.java
@@ -0,0 +1,12 @@
+package com.smartai.student.vo;
+
+import com.smartai.common.vo.BaseVO;
+
+import lombok.Data;
+
+@Data
+public class StudentParamVO extends BaseVO {
+
+    private Long id;
+    private String stName;
+}
