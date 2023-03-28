27c53bad2a4dc5dc1be0121e2bcb6c194d15289b
sunli <33113170+sunli8539@users.noreply.github.com>
2023-03-28 15:14:24 +0800

    Create BaseEntity.java

---
+++ src/main/java/com/smartai/common/entity/BaseEntity.java
@@ -0,0 +1,43 @@
+package com.smartai.common.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+public class BaseEntity implements Serializable {
+    /**
+     * id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 创建时间
+     */
+    @TableField("create_time")
+    private LocalDateTime createTime;
+
+    /**
+     * 创建人
+     */
+    @TableField("create_by")
+    private String createBy;
+
+    /**
+     * 更新时间
+     */
+    @TableField("update_time")
+    private LocalDateTime updateTime;
+
+    /**
+     * 更新人
+     */
+    @TableField("update_by")
+    private String updateBy;
+}
