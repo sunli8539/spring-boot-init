d28f5f3c3e0807017f93461903515fd2c2663a75
sunli <33113170+sunli8539@users.noreply.github.com>
2023-03-28 15:29:13 +0800

    Create ResourceOperateRecordMapper.java

---
+++ src/main/java/com/smartai/optlog/mapper/ResourceOperateRecordMapper.java
@@ -0,0 +1,11 @@
+package com.smartai.optlog.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.smartai.optlog.entity.ResourceOperateRecord;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ResourceOperateRecordMapper extends BaseMapper<ResourceOperateRecord> {
+
+}
