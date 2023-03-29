e7548710816ee871dae4543c08c15abab136559b
sunli <33113170+sunli8539@users.noreply.github.com>
2023-03-29 14:07:44 +0800

    Update SpringConfig.java

--- src/main/java/com/smartai/common/config/SpringConfig.java
+++ src/main/java/com/smartai/common/config/SpringConfig.java
@@ -1,43 +1,25 @@
-package com.smartai.common.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.web.client.RestTemplate;
-
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-
-@Configuration
-public class SpringConfig {
-
-    @Autowired
-    private RestTemplateBuilder templateBuilder;
-
-    @Bean
-    public RestTemplate restTemplate() {
-        templateBuilder.setConnectTimeout(Duration.of(30, ChronoUnit.SECONDS))
-            .setReadTimeout(Duration.of(30, ChronoUnit.SECONDS));
-        return templateBuilder.build();
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisSerializer<String> serializer = new StringRedisSerializer();
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setKeySerializer(serializer);
-        redisTemplate.setValueSerializer(serializer);
-        redisTemplate.setHashKeySerializer(serializer);
-        redisTemplate.setHashValueSerializer(serializer);
-        redisTemplate.afterPropertiesSet();
-        return redisTemplate;
-    }
-
-}
-
+// package com.smartai.common.config;
+//
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.web.client.RestTemplateBuilder;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.client.RestTemplate;
+//
+// import java.time.Duration;
+// import java.time.temporal.ChronoUnit;
+//
+// @Configuration
+// public class SpringConfig {
+//
+//     @Autowired
+//     private RestTemplateBuilder templateBuilder;
+//
+//     @Bean
+//     public RestTemplate restTemplate() {
+//         templateBuilder.setConnectTimeout(Duration.of(30, ChronoUnit.SECONDS))
+//             .setReadTimeout(Duration.of(30, ChronoUnit.SECONDS));
+//         return templateBuilder.build();
+//     }
+// }
+//
