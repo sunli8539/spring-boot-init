ae9f5dd2051b2168a2ba86a63e5d2d1289ba5942
sunli <33113170+sunli8539@users.noreply.github.com>
2023-03-30 16:41:07 +0800

    Update FunctionMapUtil.java

--- src/main/java/com/smartai/common/util/FunctionMapUtil.java
+++ src/main/java/com/smartai/common/util/FunctionMapUtil.java
@@ -4,6 +4,7 @@ import com.smartai.student.entity.Student;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 
 public class FunctionMapUtil {
@@ -22,6 +23,15 @@ public class FunctionMapUtil {
     }
 
     private String dealWulei(String name) {
+        return "";
+    }
+
+    // 调用
+    private String doSomething(Student student) {
+        Optional<Function<Student, String>> function = Optional.ofNullable(map.get(student.getStName()));
+        if (function.isPresent()) {
+            return function.get().apply(student);
+        }
         return "";
     }
 
