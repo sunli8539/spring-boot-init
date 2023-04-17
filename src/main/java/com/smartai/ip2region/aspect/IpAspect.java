69ef859671d04cc1e11323da16ea7a9bb441c444
leo <33113170+sunli8539@users.noreply.github.com>
2023-04-17 14:15:16 +0800

    Create IpAspect.java

---
+++ src/main/java/com/smartai/ip2region/aspect/IpAspect.java
@@ -0,0 +1,34 @@
+package com.smartai.ip2region.aspect;
+
+import com.smartai.common.util.CommonUtil;
+import com.smartai.common.util.HttpContextUtil;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.text.MessageFormat;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+@Aspect
+public class IpAspect {
+
+    @Pointcut("@annotation(com.smartai.ip2region.annotation.Ip)")
+    public void ip() {
+    }
+
+    @Around("ip()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        HttpServletRequest request = HttpContextUtil.getHttpServletRequest();
+        String ip = HttpContextUtil.getIpAddr(request);
+        log.info(MessageFormat.format("当前IP为:[{0}], 当前IP地址解析出来的地址为:[{1}]", ip, CommonUtil.getCityInfo(ip)));
+        return joinPoint.proceed();
+    }
+}
